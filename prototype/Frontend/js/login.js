02b325b7042b00fd9c7b642c4a78f27907b9f28f
prakriti_maurya <prakritimaurya01@gmail.com>
2024-03-10 04:19:12 +0530

    created sign-in page

---
+++ prototype/Frontend/js/login.js
@@ -0,0 +1,10 @@
+const signupBtn = document.getElementById("sign-up-btn");
+    const signinBtn = document.getElementById("Login-btn");
+    const mainContainer = document.querySelector(".container");
+
+    signupBtn.addEventListener("click", () => {
+        mainContainer.classList.toggle("change");
+    });
+    signinBtn.addEventListener("click", () => {
+        mainContainer.classList.toggle("change");
+    });
