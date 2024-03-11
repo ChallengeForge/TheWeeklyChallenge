3ded3ed895d575dc59eaa3366d4114c4a08305f0
baimamboukar <baimamboukar@gmail.com>
2024-03-11 14:21:04 +0100

    chore(backend): add `controllers` to main file

--- PWC/lib/PWC.pm
+++ PWC/lib/PWC.pm
@@ -1,11 +1,19 @@
 package PWC;
 use Dancer2;
+our $VERSION = '0.1';
+
 
 use FindBin;
 #use lib "$FindBin::Bin/../lib/Controllers";
 use Controllers::TeamController;
+use Controllers::AboutController;
+use Controllers::ChallengesController;
+use Controllers::FaqsController;
+use Controllers::HomeController;
+use Controllers::AuthController;
+use Controllers::RakuReviewController;
+use Controllers::PerlReviewController;
 
-our $VERSION = '0.1';
 
 dance;
 true;
