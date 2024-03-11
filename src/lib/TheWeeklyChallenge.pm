a0ae83f6b536482c19a6f0722b906d693a2f9a5a
baimamboukar <baimamboukar@gmail.com>
2024-03-11 19:02:57 +0100

    chore: update `packages` name in main file

--- src/lib/TheWeeklyChallenge.pm
+++ src/lib/TheWeeklyChallenge.pm
@@ -3,13 +3,14 @@ use Dancer2;
 our $VERSION = '0.1';
 
 # Import Controllers
-use  Controllers::PWC::HomeController;
-use  Controllers::PWC::AboutController;
-use  Controllers::PWC::AuthController;
-use  Controllers::PWC::TeamController;
-use Controllers::PWC::FaqsController;
-use Controllers::PWC::PerlReviewController;
-use Controllers::PWC::RakuReviewController;
+use Controllers::PWC::Home;
+use Controllers::PWC::About;
+use Controllers::PWC::Auth;
+use Controllers::PWC::Team;
+use Controllers::PWC::Challenges;
+use Controllers::PWC::FAQs;
+use Controllers::PWC::RakuReview;
+use Controllers::PWC::PerlReview;
 
 dance;
 
