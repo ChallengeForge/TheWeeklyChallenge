6b3be4fba7a03110d858217a540eabd495ca63ea
baimamboukar <baimamboukar@gmail.com>
2024-03-11 14:21:26 +0100

    chore: add `auth` controller and user model

---
+++ PWC/lib/Controllers/AuthController.pm
@@ -0,0 +1,30 @@
+package Controllers::AuthController;
+
+use Dancer2;
+
+
+get '/login' => sub {
+   
+    template 'login';
+};
+
+
+get '/signup' => sub {
+   
+    template 'signup';
+};
+
+
+del '/logout' => sub {
+   #logout logic
+    redirect '/login';
+};
+
+post '/login' => sub {
+    #login logic
+    redirect '/';
+};
+
+
+
+1;
--- PWC/lib/Models/Team.pm
+++ PWC/lib/Models/Team.pm
@@ -1,14 +1,14 @@
 package Models::Team;
 
-# Implement methods for accessing team data (e.g., database interaction)
+# Implement methods for accessing team data
 sub get_team_info {
   # Logic to retrieve team information from data source
   # ... (replace with your implementation)
   return {
     # Sample data
     members => [
-      { name => 'John Doe' },
-      { name => 'Jane Doe' },
+      { name => 'Memeber Uno' },
+      { name => 'Member Duo' },
     ],
     description => 'The A-Team of TheWeeklyChallenge',
   };
