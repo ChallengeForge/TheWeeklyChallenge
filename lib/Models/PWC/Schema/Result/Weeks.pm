735e73757abba78bf3b28fb82bfbe4436aeeb071
Matthew Idungafa <mattidungafa@gmail.com>
2024-03-16 09:58:40 +0100

    updated the folder structure and paths to the models

---
+++ lib/Models/PWC/Schema/Result/Weeks.pm
@@ -0,0 +1,21 @@
+package Models::PWC::Schema::Result::Week;
+
+use base qw/DBIx::Class::Core/;
+
+__PACKAGE__->table('weeks');
+__PACKAGE__->add_columns(
+    week_id => {
+        data_type         => 'integer',
+        is_auto_increment => 1,
+    },
+    week_name => {
+        data_type   => 'varchar',
+        size        => 255,
+        is_nullable => 0,
+    },
+);
+__PACKAGE__->set_primary_key('week_id');
+
+1;
+
+
---
+++ lib/Models/Schema.pm
@@ -0,0 +1,8 @@
+
+package Models::PWC::Schema;
+
+use base qw/DBIx::Class::Schema/;
+
+__PACKAGE__->load_namespaces();
+
+1;
