7a4bb850c1f7a23182d5a38b0c4f5698c3b6202e
Matthew Idungafa <mattidungafa@gmail.com>
2024-03-26 14:59:20 +0100

    updates

--- public/javascripts/dashboard-header-footer.js
+++ public/javascripts/dashboard-header-footer.js
@@ -158,17 +158,6 @@
       });
     }
   
-    /**
-     * Animation on scroll
-     */
-    window.addEventListener('load', () => {
-      AOS.init({
-        duration: 1000,
-        easing: "ease-in-out",
-        once: true,
-        mirror: false
-      });
-    });
   
   })()
   
