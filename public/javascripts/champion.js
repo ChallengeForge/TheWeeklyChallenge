7c5602a31ff51e430cdce6e2e6f1c7d03303eea5
baimamboukar <baimamboukar@gmail.com>
2024-03-28 20:19:48 +0100

    chore: fix all html tags issues and enforced formatting

--- public/javascripts/champion.js
+++ public/javascripts/champion.js
@@ -1,58 +1,62 @@
 /**
-   * Testimonials slider
-   */
-new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
+ * Testimonials slider
+ */
+new Swiper(".testimonials-slider", {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: "auto",
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 40,
     },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
+
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 40,
     },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 40
-      },
+  },
+});
 
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 40
-      }
-    }
-  });
+/**
+ * Porfolio isotope and filter
+ */
+window.addEventListener("load", () => {
+  let portfolioContainer = select(".portfolio-container");
+  if (portfolioContainer) {
+    let portfolioIsotope = new Isotope(portfolioContainer, {
+      itemSelector: ".portfolio-item",
+    });
 
-  /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item'
-      });
+    let portfolioFilters = select("#portfolio-flters li", true);
 
-      let portfolioFilters = select('#portfolio-flters li', true);
-
-      on('click', '#portfolio-flters li', function(e) {
+    on(
+      "click",
+      "#portfolio-flters li",
+      function (e) {
         e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
+        portfolioFilters.forEach(function (el) {
+          el.classList.remove("filter-active");
         });
-        this.classList.add('filter-active');
+        this.classList.add("filter-active");
 
         portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
+          filter: this.getAttribute("data-filter"),
         });
-        portfolioIsotope.on('arrangeComplete', function() {
-          AOS.refresh()
+        portfolioIsotope.on("arrangeComplete", function () {
+          AOS.refresh();
         });
-      }, true);
-    }
-
-  });
+      },
+      true,
+    );
+  }
+});
