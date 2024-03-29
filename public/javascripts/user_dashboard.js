6c2b045139489cfc92ba044fbb3e2b9309e1c1e1
Matthew Idungafa <mattidungafa@gmail.com>
2024-03-29 10:08:15 +0100

    reverting to recent changes

--- public/javascripts/user_dashboard.js
+++ public/javascripts/user_dashboard.js
@@ -1,5 +1,5 @@
 const hamBurger = document.querySelector(".toggle-btn");
 hamBurger.addEventListener("click", function () {
-  const sidebar = document.querySelector("#sidebar");
-  sidebar.classList.toggle("expand");
+ const sidebar = document.querySelector("#sidebar");
+ sidebar.classList.toggle("expand");
 });
