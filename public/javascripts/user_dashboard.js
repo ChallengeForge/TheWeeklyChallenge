a17989318824f9ff729ed42aa1b4619460315674
Matthew Idungafa <mattidungafa@gmail.com>
2024-03-30 12:59:35 +0100

    bug fixes

--- public/javascripts/user_dashboard.js
+++ public/javascripts/user_dashboard.js
@@ -1,5 +1,9 @@
 const hamBurger = document.querySelector(".toggle-btn");
 hamBurger.addEventListener("click", function () {
- const sidebar = document.querySelector("#sidebar");
- sidebar.classList.toggle("expand");
+    const sidebar = document.querySelector("#sidebar");
+    sidebar.classList.toggle("expand");
+    const caretIcon = document.querySelector(".toggle-btn i");
+    caretIcon.classList.toggle("fa-caret-right");
+    caretIcon.classList.toggle("fa-caret-left");
+    caretIcon.classList.toggle("black"); // Add a class to change the color to black
 });
